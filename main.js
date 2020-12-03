40acd0f1739ec3be67c00b261c5ed71a5e92f7ec
AlexSukharev <style531@gmail.com>
2020-12-03 11:25:11 +0200

    HW2

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
 // task 1
-// const citiesAndCountries = {
-// 	'Киев': 'Украина',
-// 	'Нью-Йорк': 'США',
-// 	'Амстердам': 'Нидерланды',
-// 	'Берлин': 'Германия',
-// 	'Париж': 'Франция',
-// 	'Лиссабон': 'Португалия',
-// 	'Вена': 'Австрия',
-// };
-// for (const key in citiesAndCountries) {
-// let array=[] 
-// array.push(`${key} - это ${citiesAndCountries[key]}`)
-// console.log(array);
-// }
+const citiesAndCountries = {
+	'Киев': 'Украина',
+	'Нью-Йорк': 'США',
+	'Амстердам': 'Нидерланды',
+	'Берлин': 'Германия',
+	'Париж': 'Франция',
+	'Лиссабон': 'Португалия',
+	'Вена': 'Австрия',
+};
+for (const key in citiesAndCountries) {
+let array=[] 
+array.push(`${key} - это ${citiesAndCountries[key]}`)
+console.log(array);
+}
 
 // task 2
 
-// function getArray(){
-// 	const amount = 9;
-// 	if(amount%3 !== 0) return console.log('NOT DIVIDED BY 3');
-//     let a = [];
-//     if(amount == 9){
-//     a.push([1,2,3],[4,5,6],[7,8,9])
-//     }else{(amount == 12)
-//         a.push([1,2,3],[4,5,6],[7,8,9],[10,11,12])
-//     }
-//     console.log(a);
-// }
+function getArray(){
+	const amount = 9;
+	if(amount%3 !== 0) return console.log('NOT DIVIDED BY 3');
+    let a = [];
+    if(amount == 9){
+    a.push([1,2,3],[4,5,6],[7,8,9])
+    }else{(amount == 12)
+        a.push([1,2,3],[4,5,6],[7,8,9],[10,11,12])
+    }
+    console.log(a);
+}
    
 // getArray()
 
-// task 3
-// const namesOfDays = {
-//     ru: ['','Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-// 	en: ['','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-// }
-// function getNameOfDay(lang,day){
-//     if(lang == 'ru'){
-//         console.log(namesOfDays.ru[day])
-//     }else{(lang == 'en')
-//         console.log(namesOfDays.en[day])
-//     }
-// };
+task 3
+const namesOfDays = {
+    ru: ['','Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+	en: ['','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+}
+function getNameOfDay(lang,day){
+    if(lang == 'ru'){
+        console.log(namesOfDays.ru[day])
+    }else{(lang == 'en')
+        console.log(namesOfDays.en[day])
+    }
+};
 
 // getNameOfDay('ru',6)
 
@@ -53,7 +53,7 @@
 
 // task 5
 
-    // let binaryNumber = [0,1,0,1];
-    // let string = (binaryNumber.join(''));
-    // let decimalNumber = parseInt(string,2)
-    // console.log(decimalNumber)
+    let binaryNumber = [0,1,0,1];
+    let string = (binaryNumber.join(''));
+    let decimalNumber = parseInt(string,2)
+    console.log(decimalNumber)
